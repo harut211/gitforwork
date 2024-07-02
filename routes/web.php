3c6ccb023f8d214a08855577bbdedf8dec3bd764
Developer <harutarakelyan14@gmail.com>
2024-07-02 17:53:43 +0400

    updated

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\WebController;
 
+Route::get('/', function () {
+    return view('welcome');
+});
+
 Route::prefix('post')->group(function () {
     Route::post('/create',  [PostController::class, 'create'])->name('post.create');
 });
