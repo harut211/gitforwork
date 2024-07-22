3cbfd1959d5572cbaa63ead9f3b0e8766669a222
Developer <harutarakelyan14@gmail.com>
2024-07-22 11:29:22 +0400

    updated

--- app/Http/Services/WebService.php
+++ app/Http/Services/WebService.php
@@ -3,25 +3,25 @@
 namespace App\Http\Services;
 
 use App\Models\Subscribe;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
-class WebService{
+class WebService
+{
 
-   public function subscribe($request)
-   {
-       $subscription = Subscribe::where('user_id',$request->user_id)
-           ->where('webs_id', $request->web_id)
-           ->first();
+    public function subscribe($request)
+    {
+        try {
+            $subscription = Subscribe::where('user_id', $request->user_id)
+                ->where('webs_id', $request->web_id)
+                ->firstOrFail();
+            return "You have already subscribed";
 
-       if(!$subscription) {
-           Subscribe::create([
-               "user_id" => $request->user_id,
-               "webs_id" => $request->web_id,
-           ]);
-           return "You have subscribed successfully";
-
-       } else {
-           return "You have already subscribed";
-       }
-
-   }
+        } catch (ModelNotFoundException $e) {
+            Subscribe::create([
+                "user_id" => $request->user_id,
+                "webs_id" => $request->web_id,
+            ]);
+            return "You have subscribed successfully";
+        }
+    }
 }
